6e9c249a16d5e55771fa682e3827f1f62b024a3f
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2014-10-30 23:28:32 -0400

    Prettied up website
    Added unhackathon description, borders

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    $("#email").attr('href',"mailto:philippekimurathollander@gmail.com");
+
+	
+						   
+	$("div").css("border", "3px solid red");
+    $('.banner .container h1').fadeIn( "slow", function() {
+  	});
+
+     $("#email").on( "click", function()) {
+         $(this).attr('href', 'philippekimurathollander@gmail.com');
+    });
+
+
 });​

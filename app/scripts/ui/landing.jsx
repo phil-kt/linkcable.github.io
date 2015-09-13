b2450ff0dae33dc290f83b49fcd63e2fdaa036a0
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-09-13 01:43:02 -0400

    added animations, landing page

--- app/scripts/ui/landing.jsx
+++ app/scripts/ui/landing.jsx
@@ -5,7 +5,20 @@ var Landing = React.createClass({
         return (
             <div className="landing">
                 <div className="intro">
-
+                    <div className="header">What's up?</div>
+                    <div className="details">I'm Phil</div>
+                    <div className="details">&</div>
+                    <div className="details">I'm a</div>
+                    <div className="anim-container">
+                        <div className="rotating-words">
+                            <span>designer</span>
+                            <span>coder</span>
+                            <span>gamer</span>
+                            <span>Pokemon Trainer</span>
+                            <span>Anime addict</span>
+                            <span>日本人</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
